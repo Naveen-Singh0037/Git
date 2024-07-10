b24ad0e89a9fece2cc35bc8df4c6416a8df098ba
Naveen Singh <ns37300@gmail.com>
2024-07-10 09:52:27 +0530

    all commit

--- Sum.java
+++ Sum.java
@@ -3,6 +3,6 @@ public class Sum {
     public static void main(String[] args) {
         int a =5;
         int b=4;
-        System.out.println(a+b);
+        System.out.println("sum of" + (a+b));
     }
 }
